021c1dea50be584b96059e1a0bbc05d07b3538fa
Philip <wowcxy2008@126.com>
2019-06-19 16:45:27 +0800

    Create user.ts

---
+++ src/service/user.ts
@@ -0,0 +1,19 @@
+/**
+ * 用户
+ * @author Philip
+ */
+import { provide } from 'midway';
+import { IUserService, IUserOptions, IUserResult } from '../interface';
+
+@provide('userService')
+export class UserService implements IUserService {
+
+  async getUser(options: IUserOptions): Promise<IUserResult> {
+    return {
+      id: options.id,
+      username: 'mockedName',
+      phone: '12345678901',
+      email: 'xxx.xxx@xxx.com',
+    };
+  }
+}
