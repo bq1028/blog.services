63be904b226c180394ee10420f62be53b8ec648a
Philip <wowcxy2008@126.com>
2019-06-24 11:04:10 +0800

    Update user.ts

--- src/dao/user.ts
+++ src/dao/user.ts
@@ -2,7 +2,7 @@ module.exports = app => {
     const { STRING, INTEGER } = app.Sequelize;
 
     const User = app.model.define('user', {
-        userid: { 
+        id: { 
           type: INTEGER, 
           primaryKey: true, 
           autoIncrement: true,
