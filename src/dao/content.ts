f9fa6fbb17a042e1ae753693ef46409daba284e7
Philip <wowcxy2008@126.com>
2019-06-24 11:05:33 +0800

    Update content.ts

--- src/dao/content.ts
+++ src/dao/content.ts
@@ -1,1 +1,23 @@
+/**
+ * 内容 dao
+ * @author Philip
+ */
 
+module.exports = app => {
+    const { STRING, INTEGER } = app.Sequelize;
+
+    const User = app.model.define('content', {
+        id: { 
+          type: INTEGER, 
+          primaryKey: true, 
+          autoIncrement: true
+        },
+        username: STRING(50),
+        userpass: STRING(32)
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    });
+
+    return User;
+};
