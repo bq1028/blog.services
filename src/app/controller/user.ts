c7fe7dd858dde111b9b6d80eac2d7301671ff11e
Philip <wowcxy2008@126.com>
2019-07-03 16:17:44 +0800

    Update user.ts

--- src/app/controller/user.ts
+++ src/app/controller/user.ts
@@ -1,5 +1,9 @@
+/**
+ * 用户控制器
+ * @author Philip
+ */
 import { controller, get, inject, provide } from 'midway';
-import { IUserService, IUserResult } from '../../interface';
+import { User, IUserResult } from '../../interface';
 
 @provide()
 @controller('/user')
@@ -7,14 +11,17 @@ export class UserController {
     @inject()
     private userService;
 
-  @get('/:id')
-  async getUser(ctx): Promise<void> {
-    const id: number = ctx.params.id;
-    const user: IUserResult = await this.service.getUser({id});
-    ctx.body = {
-      success: true, 
-      message: 'OK', 
-      data: user
-    };
+    @post()
+    async login(ctx): Promise<void> {
+        const { username, password } = ctx.params;
+        const user: User = await this.userService.login({
+            username,
+            password,
+        });
+        
+        ctx.body = {
+            success: true, 
+            message: 'OK'
+        };
   }
 }
